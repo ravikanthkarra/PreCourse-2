4edd19e915ad03dc32c71b7fc862261635985964
Ravikanth Karra <ravikanthkarra@System.local>
2024-06-16 12:58:13 -0400

    Done precourse2

--- Exercise_1.py
+++ Exercise_1.py
@@ -6,9 +6,27 @@
 def binarySearch(arr, l, r, x): 
   
   #write your code here
-  
-    
-  
+    return helperbinSearch(arr, l, r, x)
+
+
+
+def helperbinSearch(arr, l, r, x):
+
+    if l > r:
+        return -1
+
+    mid = (l+r) // 2
+
+    if x == arr[mid]:
+        return mid
+    elif x > arr[mid]:
+      l = mid+1
+      return helperbinSearch(arr, l, r, x)
+    else:
+      r = mid-1
+      return helperbinSearch(arr, l, r, x)
+
+
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
 x = 10
@@ -17,6 +35,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print(f"Element is present at index {result}")
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
